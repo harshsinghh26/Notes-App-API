4dfb13c546111a35281bb6069ac502625a963a49
harshsinghh26 <harshsi343@gmail.com>
2025-04-10 20:53:55 +0530

    fix get all notes empty data error

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -11,7 +11,7 @@ import {
 const router = Router();
 
 router.route('/createnotes').post(verifyJWT, createNotes);
-router.route('/getnotes').get(verifyJWT, getNotes);
+router.route('/getnotes').get(getNotes);
 router.route('/getnotesbyid/:id').get(verifyJWT, getNotesById);
 router.route('/update/:id').put(verifyJWT, updateNotes);
 router.route('/delete/:id').delete(verifyJWT, deleteNotes);
